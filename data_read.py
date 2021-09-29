96476555d0c42be381f795633964e8c705051d78
akash <akash.chak1999@gmail.com>
2021-09-29 22:34:46 +0530

    Reading and parsing into strings

--- data_read.py
+++ data_read.py
@@ -1,7 +1,24 @@
 import json
-
+from lpu_usage import get_lpu_usage
+from memory_usage import show_mem_data
 
 def read_data():
-    f=open("data.json","r")
-    out=f.read()
-    print(out)
+
+    #Memory Usage
+    memdata = show_mem_data()
+    memdict=json.loads(memdata)
+
+    
+
+
+
+
+    #Total Memory
+    print("Total Memory: {}".format(memdict["MEMORY USAGE"]["Total Memory"]))
+    #Memory In USe
+    print("In Use Memory: {}".format(memdict["MEMORY USAGE"]["Total Used Memory"]))
+    #Available Memory
+    print("Aailable Memory: {}".format(memdict["MEMORY USAGE"]["Total Available Memory"]))
+    
+
+read_data()
